6dcb6c1dc8bf762d12389a301ac89cf59d58cb36
Saruman21rus <saruman21rus@gmail.com>
2013-07-06 21:12:53 +0400

    first

---
+++ FirsMavenProject/my-webapp/src/main/java/com/mycompany/app/myFirstTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+public class myFirstTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(getHelloWorld());
+	}
+ 
+	public static String getHelloWorld() { 
+		return "Hello World";
+	}
+ 
+	public static String getHelloWorld2() {
+		return "Hello World 2";
+	}
+}
---
+++ FirsMavenProject/my-webapp/src/test/java/com/mycompany/app/TestmyFirstTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestmyFirstTest {
+
+	@Test
+	public void test() {
+		assertEquals(myFirstTest.getHelloWorld(), "Hello World");
+	}
+
+}
